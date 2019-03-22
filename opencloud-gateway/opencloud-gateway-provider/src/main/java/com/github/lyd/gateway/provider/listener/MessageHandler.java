6fd2b7d816fadbba33f2e2dbec184ca53f6a4c52
liu.yadu <515608851@qq.com>
2019-03-23 02:19:14 +0800

    增加IP库

--- opencloud-gateway/opencloud-gateway-provider/src/main/java/com/github/lyd/gateway/provider/listener/MessageHandler.java
+++ opencloud-gateway/opencloud-gateway-provider/src/main/java/com/github/lyd/gateway/provider/listener/MessageHandler.java
@@ -4,13 +4,20 @@ import com.github.lyd.common.constants.MqConstants;
 import com.github.lyd.common.utils.BeanConvertUtils;
 import com.github.lyd.gateway.client.model.entity.GatewayAccessLogs;
 import com.github.lyd.gateway.provider.mapper.GatewayLogsMapper;
-import com.github.lyd.gateway.provider.utils.IpRegionUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.lionsoul.ip2region.DataBlock;
+import org.lionsoul.ip2region.DbConfig;
+import org.lionsoul.ip2region.DbSearcher;
+import org.lionsoul.ip2region.Util;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.handler.annotation.Payload;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
@@ -26,6 +33,81 @@ public class MessageHandler {
     private GatewayLogsMapper gatewayLogsMapper;
 
     /**
+     * 临时存放减少io
+     */
+    private byte[] bytes = null;
+
+
+    public String getRegion(String ip) {
+        try {
+            //db
+            InputStream inputStream = MessageHandler.class.getClassLoader().getResourceAsStream("data/ip2region.db");
+            if (bytes == null) {
+                bytes = read(inputStream);
+            }
+
+            if (bytes == null) {
+                return null;
+            }
+            //查询算法
+            int algorithm = DbSearcher.MEMORY_ALGORITYM; //B-tree
+            //DbSearcher.BINARY_ALGORITHM //Binary
+            //DbSearcher.MEMORY_ALGORITYM //Memory
+            try {
+                DbConfig config = new DbConfig();
+                DbSearcher searcher = new DbSearcher(config, bytes);
+
+                //define the method
+                Method method = null;
+                switch (algorithm) {
+                    case DbSearcher.BTREE_ALGORITHM:
+                        method = searcher.getClass().getMethod("btreeSearch", String.class);
+                        break;
+                    case DbSearcher.BINARY_ALGORITHM:
+                        method = searcher.getClass().getMethod("binarySearch", String.class);
+                        break;
+                    case DbSearcher.MEMORY_ALGORITYM:
+                        method = searcher.getClass().getMethod("memorySearch", String.class);
+                        break;
+                }
+
+                DataBlock dataBlock = null;
+                if (Util.isIpAddress(ip) == false) {
+                    log.error("Error: Invalid ip address");
+                }
+
+                dataBlock = (DataBlock) method.invoke(searcher, ip);
+
+                return dataBlock.getRegion();
+
+            } catch (Exception e) {
+                log.error("Error:{}", e);
+            }
+        } catch (Exception e) {
+            log.error("Error:{}", e);
+        }
+        return null;
+    }
+
+    public static byte[] read(InputStream inputStream) throws IOException {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int num = inputStream.read(buffer);
+            while (num != -1) {
+                baos.write(buffer, 0, num);
+                num = inputStream.read(buffer);
+            }
+            baos.flush();
+            return baos.toByteArray();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
+
+    /**
      * 接收访问日志
      *
      * @param access
@@ -36,8 +118,8 @@ public class MessageHandler {
             if (access != null) {
                 GatewayAccessLogs logs = BeanConvertUtils.mapToObject(access, GatewayAccessLogs.class);
                 if (logs != null && logs.getAccessId() != null) {
-                    if(logs.getIp()!=null){
-                        logs.setRegion(IpRegionUtils.getRegion(logs.getIp()));
+                    if (logs.getIp() != null) {
+                        logs.setRegion(getRegion(logs.getIp()));
                     }
                     logs.setUseTime(logs.getResponseTime().getTime() - logs.getRequestTime().getTime());
                     gatewayLogsMapper.insertSelective(logs);
