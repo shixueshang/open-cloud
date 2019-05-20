40b0ebefc0c3acc852e4c03d52ba467ffc18a8cb
liuyadu <515608851@qq.com>
2019-05-20 16:31:28 +0800

    增加jackson自动配置类

--- opencloud-common/opencloud-common-starter/src/main/java/com/opencloud/autoconfigure/WebMvcConfiguration.java
+++ opencloud-common/opencloud-common-starter/src/main/java/com/opencloud/autoconfigure/WebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package com.opencloud.autoconfigure;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,25 +9,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author liuyadu
- * @Order(101)排序很重要,否则多个WebSecurityConfigurerAdapter会报错
  */
-@Order(101)
-public class WebMvcConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(
-                "/",
-                "/error",
-                "/static/**",
-                "/**/v2/api-docs/**",
-                "/**/swagger-resources/**",
-                "/webjars/**",
-                "/swagger-ui.html",
-                "/doc.html",
-                "/favicon.ico");
+    /**
+     * 多个WebSecurityConfigurerAdapter 需要加@Order
+     */
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers(
+                    "/",
+                    "/error",
+                    "/static/**",
+                    "/**/v2/api-docs/**",
+                    "/**/swagger-resources/**",
+                    "/webjars/**",
+                    "/swagger-ui.html",
+                    "/doc.html",
+                    "/favicon.ico");
+        }
     }
+
 
     /**
      * 资源处理器
@@ -36,7 +43,7 @@ public class WebMvcConfiguration extends WebSecurityConfigurerAdapter implements
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("swagger-ui.html","doc.html")
+        registry.addResourceHandler("swagger-ui.html", "doc.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
