9c780ea5f688419114e78bc26ab9473faf791d97
liuyadu <515608851@qq.com>
2019-07-19 18:42:45 +0800

    修改下载方式

--- platform/open-cloud-generator-server/src/main/java/com/opencloud/generator/server/controller/GenerateController.java
+++ platform/open-cloud-generator-server/src/main/java/com/opencloud/generator/server/controller/GenerateController.java
@@ -4,23 +4,22 @@ import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.generator.config.*;
 import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.google.common.collect.Maps;
 import com.opencloud.common.model.ResultBody;
 import com.opencloud.common.utils.DateUtils;
-import com.opencloud.common.utils.WebUtils;
 import com.opencloud.generator.server.service.GenerateConfig;
 import com.opencloud.generator.server.service.GeneratorService;
 import com.opencloud.generator.server.utils.ZipUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.io.FileUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.File;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author: liuyadu
@@ -62,8 +61,8 @@ public class GenerateController {
 
 
     @ApiOperation(value = "代码生成并下载", notes = "代码生成并下载")
-    @PostMapping("/download")
-    public ResultBody<List<TableInfo>> download(
+    @PostMapping("/execute")
+    public ResultBody<List<TableInfo>> execute(
             @RequestParam(value = "type") String type,
             @RequestParam(value = "driverName") String driverName,
             @RequestParam(value = "url") String url,
@@ -73,8 +72,7 @@ public class GenerateController {
             @RequestParam(value = "parentPackage") String parentPackage,
             @RequestParam(value = "moduleName") String moduleName,
             @RequestParam(value = "includeTables") String includeTables,
-            @RequestParam(value = "tablePrefix") String tablePrefix,
-            HttpServletResponse response
+            @RequestParam(value = "tablePrefix") String tablePrefix
     ) throws Exception {
         String outputDir = System.getProperty("user.dir") + File.separator + "temp" + File.separator + DateUtils.getCurrentTimestampStr();
         GenerateConfig config = new GenerateConfig();
@@ -95,30 +93,11 @@ public class GenerateController {
         // 压缩目录
         String[] srcDir = {outputDir};
         ZipUtil.toZip(srcDir, filePath, true);
-        download(response, filePath, fileName);
-        // 删除目录
-        FileUtils.deleteDirectory(new File(outputDir));
+        Map data = Maps.newHashMap();
+        data.put("filePath",filePath);
+        data.put("fileName",fileName);
         return ResultBody.ok();
     }
 
-    /**
-     * 文件下载
-     *
-     * @param response
-     * @param filePath
-     * @param fileName
-     * @throws IOException
-     */
-    private void download(HttpServletResponse response, String filePath, String fileName) throws IOException {
-        WebUtils.setFileDownloadHeader(response, fileName);
-        BufferedInputStream inStream = new BufferedInputStream(new FileInputStream(filePath));
-        BufferedOutputStream outStream = new BufferedOutputStream(response.getOutputStream());
-        byte[] buffer = new byte[1024];
-        int bytesRead = 0;
-        while ((bytesRead = inStream.read(buffer)) != -1) {
-            outStream.write(buffer, 0, bytesRead);
-        }
-        outStream.flush();
-        inStream.close();
-    }
+
 }
