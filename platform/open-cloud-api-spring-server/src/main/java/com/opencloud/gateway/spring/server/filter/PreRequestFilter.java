6a94ccec22665724d58a2443722a97bb470f1f65
liuyadu <515608851@qq.com>
2019-08-28 13:09:07 +0800

    修复添加请求头报错问题

--- platform/open-cloud-api-spring-server/src/main/java/com/opencloud/gateway/spring/server/filter/PreRequestFilter.java
+++ platform/open-cloud-api-spring-server/src/main/java/com/opencloud/gateway/spring/server/filter/PreRequestFilter.java
@@ -2,6 +2,8 @@ package com.opencloud.gateway.spring.server.filter;
 
 import com.opencloud.common.interceptor.FeignRequestInterceptor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -20,13 +22,16 @@ public class PreRequestFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        // 添加请求时间
-        exchange.getAttributes().put("requestTime", new Date());
         // 添加自定义请求头
         String rid = UUID.randomUUID().toString();
-        exchange.getRequest().getHeaders().set(FeignRequestInterceptor.X_REQUEST_ID, rid);
-        exchange.getResponse().getHeaders().set(FeignRequestInterceptor.X_REQUEST_ID,rid);
-        return chain.filter(exchange);
+        ServerHttpRequest request  = exchange.getRequest().mutate().header(FeignRequestInterceptor.X_REQUEST_ID, rid).build();
+        ServerHttpResponse response = exchange.getResponse();
+        response.getHeaders().set(FeignRequestInterceptor.X_REQUEST_ID,rid);
+        //将现在的request 变成 change对象
+        ServerWebExchange build = exchange.mutate().request(request).response(response).build();
+        // 添加请求时间
+        build.getAttributes().put("requestTime", new Date());
+        return chain.filter(build);
     }
 
 
