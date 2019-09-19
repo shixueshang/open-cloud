2e82112a51097a21bb74ca0a4bd8203c0f8e762a
liuyadu <515608851@qq.com>
2019-09-19 14:30:21 +0800

    增加body缓存

--- components/open-cloud-common-core/src/main/java/com/opencloud/common/filter/XssServletRequestWrapper.java
+++ components/open-cloud-common-core/src/main/java/com/opencloud/common/filter/XssServletRequestWrapper.java
@@ -2,9 +2,14 @@ package com.opencloud.common.filter;
 
 
 import com.opencloud.common.utils.StringUtils;
+import org.apache.commons.io.IOUtils;
 
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.*;
+
 /**
  * xss 过滤
  * body 缓存
@@ -13,10 +18,47 @@ import javax.servlet.http.HttpServletRequestWrapper;
  */
 public class XssServletRequestWrapper extends HttpServletRequestWrapper {
     private HttpServletRequest request;
+    private final byte[] body;
 
-    public XssServletRequestWrapper(HttpServletRequest request) {
+
+    public XssServletRequestWrapper(HttpServletRequest request) throws IOException {
         super(request);
         this.request = request;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        IOUtils.copy(request.getInputStream(), baos);
+        this.body = baos.toByteArray();
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(getInputStream()));
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        final ByteArrayInputStream bais = new ByteArrayInputStream(body);
+        return new ServletInputStream() {
+
+            @Override
+            public int read() throws IOException {
+                return bais.read();
+            }
+
+            @Override
+            public boolean isFinished() {
+                return false;
+            }
+
+            @Override
+            public boolean isReady() {
+                return false;
+            }
+
+            @Override
+            public void setReadListener(ReadListener readListener) {
+
+            }
+        };
     }
 
     @Override
